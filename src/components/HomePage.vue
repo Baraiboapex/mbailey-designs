60da24732a2f741500253fd79974c5f804a8755a
matth <matth@BARAIBOBOX>
2024-08-30 13:12:33 -0400

    init

---
+++ src/components/HomePage.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+  </div>
+</template>
+<script setup>
+  import { reactive } from "vue";
+
+  const state = reactive({
+    introData:{},
+    recentProjects:[],
+    recentBlogPosts:[]
+  });
+
+  
+</script>
